26aa45f8d11921fd22f38505456cc789e5c9b266
+ <+>
2022-10-10 16:03:23 +0300

    commit tree 6670883e11c1c65000d097b350799f20222bdf87

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,18 @@
-class Bird < ApplicationRecord
-end
+class BirdsController < ApplicationController
+
+    # POST /birds
+    def create
+      bird = Bird.create(bird_params)
+      render json: bird, status: :created
+    end
+  
+    # other controller actions here
+  
+    private
+    # all methods below here are private
+  
+    def bird_params
+      params.permit(:name, :species)
+    end
+  
+  end
